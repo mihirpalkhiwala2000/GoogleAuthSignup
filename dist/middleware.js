c1e359318e21ea7233687bb5d739b8207810a325
Mihirpalkhiwala2000 <palkhiwalamihir@gmail.com>
2023-08-24 12:29:11 +0530

    first

--- dist/middleware.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isLoggedIn = void 0;
-const isLoggedIn = (req, res, next) => {
-    req.user ? next() : res.sendStatus(401);
-};
-exports.isLoggedIn = isLoggedIn;
-//# sourceMappingURL=middleware.js.map
