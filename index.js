e70a1e791bd083af14561f699aa5713887864664
Mihirpalkhiwala2000 <palkhiwalamihir@gmail.com>
2023-08-24 10:17:14 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const passport = require("passport");
+var session = require("express-session");
+require("./auth");
+
+const app = express();
+app.use(session({ secret: "secretCode" }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+function isLoggedIn(req, res, next) {
+  req.user ? next() : res.sendStatus(401);
+}
+app.get("/", (req, res) => {
+  res.send('<a href="/auth/google">Authenticate with google</a>');
+});
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "/protected",
+    failureRedirect: "/auth/failure",
+  })
+);
+
+app.get("/auth/failure", (req, res) => {
+  res.send("Something went wrong");
+});
+
+app.get("/protected", isLoggedIn, (req, res) => {
+  const name = req.user.displayName;
+  res.send(`hello ${name}`);
+});
+
+app.get("/logout", (req, res) => {
+  req.session.destroy();
+  res.send("Goodbye!!");
+});
+
+app.listen(3000, () => {
+  console.log("Connected");
+});
